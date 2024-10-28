a61eb06c44c634a4ab90b7f9138482bac6a52420
Muhammad Umar <umar.dms@gmail.com>
2024-10-28 20:50:27 +0500

    Visit my portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.jpg', '**/*.JPG'], // Add both lowercase and uppercase extensions
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, './src/assets'),
+    },
+  },
 });
